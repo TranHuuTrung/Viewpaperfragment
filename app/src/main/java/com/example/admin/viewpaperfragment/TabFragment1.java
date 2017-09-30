d79aa1ddf2b01857bf34bbc9b6fcb22d5e061f31
TranHuuTrung <huutrungbkdn15@gmail.com>
2017-09-30 20:54:23 +0700

    cm

--- app/src/main/java/com/example/admin/viewpaperfragment/TabFragment1.java
+++ app/src/main/java/com/example/admin/viewpaperfragment/TabFragment1.java
@@ -20,13 +20,7 @@ public class TabFragment1 extends Fragment {
     public TabFragment1() {
         // Required empty public constructor
     }
-   /* @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_fragment1, container, false);
-    }
-*/
+
   @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
